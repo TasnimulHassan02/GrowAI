b44bbee3f664872246d347fc48d5c0dab485d2d7
Tasnimul Hassan <tasnimulhassan02@gmail.com>
2025-12-12 21:48:36 +0600

    Initial project frontend setup

---
+++ client/src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { useState } from 'react'
+import Navbar from './components/Navbar'
+import HomePage from './pages/HomePage'
+import LoginPage from './pages/LoginPage'
+import RegisterPage from './pages/RegisterPage'
+import { Routes, Route} from 'react-router-dom'
+
+function App() {
+  
+
+  return (
+   
+      <div className='min-h-screen bg-blue-200 transition-colors duration-500'>
+        <Navbar /> 
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/login' element={<LoginPage />} />
+          <Route path='/register' element={<RegisterPage />} />
+        </Routes>
+      
+      </div>
+      
+  )
+}
+
+export default App
