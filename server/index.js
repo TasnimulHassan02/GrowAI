f4ec75c652824655932a5d974fe1a820af7cfc5b
Tasnimul Hassan <tasnimulhassan02@gmail.com>
2025-12-12 14:33:19 +0600

    Updated backend setup

--- server/index.js
+++ server/index.js
@@ -1,14 +1,24 @@
 import express from 'express';
+import helmet from 'helmet';
+import morgan from 'morgan';   
+import cors from 'cors';
+import dotenv from 'dotenv';
 
-
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT;
+
+app.use(helmet());
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
 
-  console.log('Server is running on port 3000');
+  console.log(`Server is running on port ${PORT}`);
 });
